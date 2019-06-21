406a3252cf66b6eaf7f72f377a2e6b01ac41cb3c
BrunoMSPais <bpais@outlook.pt>
2019-06-21 17:49:33 +0100

    projeto 011: commit final

--- Temporada2-Javascript/pm-proj011/JS/comportamento.js
+++ Temporada2-Javascript/pm-proj011/JS/comportamento.js
@@ -1,69 +1,51 @@
-//var peso = document.querySelector('.peso');
+let botao = document.querySelector("button"); //define a variavel botao
 
-//var altura = document.querySelector('.altura');
+function validarBtn() { //verifica se os campos estam preenchidos
+    if (altura.value == "") {
+        altura.focus(); //reenvia o cursor para o campo em falta
+        return false;
+    }else if (peso.value == "") {
+        peso.focus(); //reenvia o cursor para o campo em falta
+        return false;
+    }else botao.addEventListener("click", function(){ //após verificar passa ao evento seguinte - clicar no botão
+   
+        function calcularImc(peso , altura) { //função para calcular o imc com as variaveis declaradas entre parentesis
+            
+            altura = document.getElementById("altura").value; //define a variavel altura
+            peso = document.getElementById("peso").value; //define a variavel peso
+        
+            return (peso / (altura * altura)); //retorna o resultado do calculo
+        }
+        let resultado = calcularImc(); //armazena o valor retornado na variavel resultado
+        let imc = parseFloat(resultado.toFixed(2)); //define a variavel imc apenas com duas casas decimais da variavel resultado
 
-var peso = document.getElementsByClassName('peso');
+        botao.style.display = 'none';  //remove o botão
 
-var altura = document.getElementsByClassName('altura');
-
-var botao = document.getElementsByClassName('botao');
-
-var imc = peso / (altura * altura)
-
-function habilitarBtn() {
-    var pesoIn = document.getElementsByClassName('peso').require;
-    var alturaIn = document.getElementsByClassName('altura').require;
-
-    if (pesoIn = Number)
-        if (!document.getElementById('imc').disabled) document.getElementById('imc').disabled = false;
-
-    if (alturaIn = Number)
-        if (!document.getElementById('imc').disabled) document.getElementById('imc').disabled = false;
-}
-var btn = document.getElementsByClassName('btn');
-
-bnt.addEventListener('click', ocultar)
-
-function ocultar() {
-    this.style.display = 'none'
-}
-
-if (imc < 16) {
-    document.querySelector('.texto') == "Magreza grave";
-    alert(imc, 'Magreza grave');
-}
-
-if (16 = < imc < 17) {
-    document.querySelector('.texto') == "Magreza moderada";
-    alert(imc, 'Magraza moderada');
-}
-
-if (17 = < imc < 18.5) {
-    document.querySelector('.texto') == "Magreza leve";
-    alert(imc, 'Magreza leve');
-}
-
-if (18.5 = < imc < 25) {
-    document.querySelector('.texto') == "Saudável";
-    alert(imc, 'Saudável');
-}
-
-if (imc = 25, imc < 30) {
-    document.querySelector('.texto') == "Sobrepeso";
-    alert(imc, 'Sobrepeso');
-}
-
-if (imc = 30, imc < 35) {
-    document.querySelector('.texto') == "Obesidade";
-    alert(imc, 'Obesidade');
-}
-
-if (imc = 35, imc < 40) {
-    document.querySelector('.texto') == "Obesidade Grave";
-    alert(imc, 'Obesidade Grave');
-}
-
-if (imc = 40, imc > 40) {
-    document.querySelector('.texto') == "Obesidade mórbida";
-    alert(imc, 'Obesidade mórbida');
+        let valor = document.querySelector("span"); //define a variavel valor e associa ao campo html span
+        valor.innerHTML = imc;  //insere o valor imc no elemento span, tornando-o visivel na tela
+    
+        function descricao() {  //define os intrevalos de valor de imc e a mensagem a exibir
+            if (imc < 16) {
+                return "Magreza Grave"
+            } else if ((16 <= imc) && (imc < 17)) {
+                return "Magreza Moderada"
+            } else if ((17 <= imc) && (imc < 18.5)) {
+                return "Magreza Leve"
+            } else if ((18.5 <= imc) && (imc < 25)) {
+                return "Saudável"
+            } else if ((25 <= imc) && (imc < 30)) {
+                return "Sobrepeso"
+            } else if ((30 <= imc) && (imc < 35)) {
+                return "Obesidade"
+            } else if ((35 <= imc) && (imc < 40)) {
+                return "Obesidade Severa"
+            } else if (imc >= 40) {
+                return "Obesidade Mórbida"
+            }
+        };
+        
+        let texto = document.querySelector("small");  //define a variavel text e associa ao elemento small
+        texto.innerHTML = descricao();  //insere a mensagem no elemento small e exibe-a na tela
+    });
+    
 }
